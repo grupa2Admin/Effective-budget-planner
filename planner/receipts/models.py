aaf0810c25cacfc468aad6a0d6bf946730f9968a
bart <bartosz.kwiatk@gmail.com>
2021-06-03 16:40:04 +0200

    new changes

--- planner/receipts/models.py
+++ planner/receipts/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 class Receipt(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
     title = models.CharField(max_length=200)
-    description = models.TextField(null=True, blank=True)
+    description = models.CharField(max_length=300, null=True, blank=True)
     date = models.DateField()
     image = models.ImageField(upload_to="img/%y")
 
--- planner/receipts/views.py
+++ planner/receipts/views.py
@@ -5,30 +5,41 @@ from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
 
-class ReceiptsList(ListView):
+
+class ReceiptsList(LoginRequiredMixin, ListView):
     model = Receipt
     context_object_name = 'receipts'
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['receipts'] = context['receipts'].filter(user=self.request.user)
+        return context
 
-class ReceiptDetail(DetailView):
+
+class ReceiptDetail(LoginRequiredMixin, DetailView):
     model = Receipt
     context_object_name = 'receipt'
 
 
-class ReceiptCreate(CreateView):
+class ReceiptCreate(LoginRequiredMixin, CreateView):
     model = Receipt
-    fields = '__all__'
+    fields = ['title', 'description', 'date', 'image']
+    success_url = reverse_lazy('receipts')
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super(ReceiptCreate, self).form_valid(form)
+
+
+class ReceiptUpdate(LoginRequiredMixin, UpdateView):
+    model = Receipt
+    fields = ['title', 'description', 'date', 'image']
     success_url = reverse_lazy('receipts')
 
 
-class ReceiptUpdate(UpdateView):
-    model = Receipt
-    fields = '__all__'
-    success_url = reverse_lazy('receipts')
-
-
-class DeleteView(DeleteView):
+class DeleteView(LoginRequiredMixin, DeleteView):
     model = Receipt
     context_object_name = 'receipt'
     success_url = reverse_lazy('receipts')
