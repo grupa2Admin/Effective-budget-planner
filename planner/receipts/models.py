47ab5cf25e48c0bd7f15ebe91384003a470cbaee
bart <bartosz.kwiatk@gmail.com>
2021-06-08 21:28:22 +0200

    search button

--- planner/receipts/models.py
+++ planner/receipts/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 class Receipt(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
     title = models.CharField(max_length=200)
